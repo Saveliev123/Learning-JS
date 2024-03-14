7a365ddf322a45250e7f1b03fcffef04c07469e9
Kirill Saveliev <64482652+Saveliev123@users.noreply.github.com>
2024-03-14 20:09:30 +0500

    Create Задача 2.js

---
+++ 5. Типы данных/4. Массивы/Задача 2.js
@@ -0,0 +1,10 @@
+let arr = ["Джаз", "Блюз"];
+alert(arr);
+arr.push("Рок-н-ролл");
+alert(arr);
+arr[(arr.length / 2) - 0.5] = "Классика";
+alert(arr);
+arr.shift();
+alert(arr);
+arr.unshift("Рэп", "Регги");
+alert(arr);
