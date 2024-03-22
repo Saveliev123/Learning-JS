d34afb685352f47708a13f414becc05657c3e124
Kirill Saveliev <64482652+Saveliev123@users.noreply.github.com>
2024-03-22 14:31:30 +0500

    Create Задача 3.js

---
+++ 6. Продвинутая работа с функциями/1. Рекурсия и стек/Задача 3.js
@@ -0,0 +1,23 @@
+// Неоптимизированно
+function fib(n)
+{
+  return (n == 1 || n == 2) ? 1 : fib(n - 1) + fib(n - 2);
+}
+
+// Оптимизированно
+function fib(n) 
+{
+  let a = 1;
+  let b = 1;
+  for (let i = 3; i <= n; i++) 
+  {
+    let c = a + b;
+    a = b;
+    b = c;
+  }
+  return b;
+}
+
+alert(fib(3)); // 2
+alert(fib(7)); // 13
+alert(fib(77)); // 5527939700884757
